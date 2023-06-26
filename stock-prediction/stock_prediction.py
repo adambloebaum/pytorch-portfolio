6739b15a1774d75b07732011541173eb34465bbe
adambloebaum <96801448+adambloebaum@users.noreply.github.com>
2023-06-26 16:47:50 -0700

    Create stock_prediction.py

---
+++ stock-prediction/stock_prediction.py
@@ -0,0 +1,11 @@
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import pandas as pd
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Seaborn plot styling
+sns.set(style = 'white', font_scale = 2)
